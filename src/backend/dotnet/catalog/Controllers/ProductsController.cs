06667a1004116a73f3927775a8dddb79857160ab
Mukesh Murugan <iammukeshm@gmail.com>
2023-02-28 01:04:23 +0530

    added dummy endpoint

---
+++ src/backend/dotnet/catalog/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerlessHero.Lambdas.Catalog.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello");
+    }
+}
