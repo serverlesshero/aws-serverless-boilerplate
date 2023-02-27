0750b6c81285a2ebc2822580026f9a461c26522b
Mukesh Murugan <iammukeshm@gmail.com>
2023-02-27 15:47:16 +0530

    minor changes

--- deployments/terraform/backend/s3.tf
+++ deployments/terraform/backend/s3.tf
@@ -7,3 +7,13 @@ resource "aws_s3_bucket" "s3_bucket" {
     prevent_destroy = true
   }
 }
+
+resource "aws_s3_bucket" "s3_bucket_for_releases" {
+  bucket = "serverlesshero-releases"
+  tags = {
+    Name = "serverlesshero-releases"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
